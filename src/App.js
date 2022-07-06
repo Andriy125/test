2929cadabb90d424bef6b977fc76ea7b623ef89d
Andriy125 <57405296+Andriy125@users.noreply.github.com>
2022-07-06 16:23:32 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ви коб знали скільки на це сльоз і сил пішло
+        </p>
+        <p>
+          Але воно запрацювало)))
         </p>
         <a
           className="App-link"
